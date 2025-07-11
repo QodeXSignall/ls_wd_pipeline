726837d7c90e9c395e03fd381fbc1cb2b69a9ec7
Your Name <you@example.com>
2025-07-11 15:34:55 +0500

    Порт скорректирован

--- ls_wb_pipeline/fastapi_app/main.py
+++ ls_wb_pipeline/fastapi_app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from ls_wb_pipeline.fastapi_app.routes import router
+import uvicorn
 
 app = FastAPI(title="LS WebDAV Pipeline API")
 
 app.include_router(router)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8082, reload=True)
+    uvicorn.run(...)
