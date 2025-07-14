d84dd9a17420ebf3980e3428ce0094175e2d862c
Your Name <you@example.com>
2025-07-14 11:24:50 +0500

    В демон добавлена автоматическая синхронизация labelstudio

--- mount_webdav.py
+++ mount_webdav.py
@@ -1,4 +1,4 @@
-from ls_wb_pipeline.functions import remount_webdav
+from ls_wb_pipeline.functions import remount_webdav, sync_label_studio_storage
 import time
 
 
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     parser.add_argument("--from-systemd", action="store_true", help="Не использовать --daemon")
     args = parser.parse_args()
     remount_webdav(from_systemd=args.from_systemd)
+    sync_label_studio_storage()
     time.sleep(36000)
