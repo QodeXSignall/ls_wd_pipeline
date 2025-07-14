3f29ecd5f9969e63a3a36f3de6d9d78b51193a28
Your Name <you@example.com>
2025-07-14 14:46:17 +0500

    Фиксы

--- ls_wb_pipeline/fastapi_app/services.py
+++ ls_wb_pipeline/fastapi_app/services.py
@@ -13,7 +13,7 @@ def analyze_dataset_service():
 
 
 def cleanup_frames_tasks(json_data: bytes = None, dry_run:bool = False, save_annotated: bool = True):
-    all_tasks, deleted_tasks, saved_amount = functions.delete_ls_tasks(dry_run=dry_run, save_annoted=save_annotated)
+    all_tasks, deleted_tasks, saved_amount = functions.delete_ls_tasks(dry_run=dry_run, save_annotated=save_annotated)
     if json_data:
         all_tasks = json.loads(json_data)
     deleted_files_report = functions.clean_cloud_files_from_tasks(tasks=all_tasks, dry_run=dry_run)
