3eef223ae70af299411bc8cdef33b38141bd89bd
Your Name <you@example.com>
2025-07-14 16:18:09 +0500

    Рефакторинг поиска по имени

--- ls_wb_pipeline/functions.py
+++ ls_wb_pipeline/functions.py
@@ -496,9 +496,10 @@ def process_video_loop(max_frames=3000, only_cargo_type: str = None, fps: float 
             reg_folder, other = parse_video_name(concrete_video_name)
             remote_dir = f"{BASE_REMOTE_DIR}/{reg_folder}/{other.replace('.mp4', '') if other.endswith('.mp4') else other}"
             remote_dir = unquote(remote_dir)
-        except:
+        except Exception as e:
             return {
-                "error": f"Не удалось распарсить название видео {concrete_video_name}. Убедитесь, что он в формате REGID_Y.m.d H.M.S-H.M.S.mp4"}
+                "error": f"Не удалось распарсить название видео {concrete_video_name}: {e}. "
+                         f"Убедитесь, что он в формате REGID_Y.m.d H.M.S-H.M.S.mp4"}
     else:
         remote_dir = BASE_REMOTE_DIR
 
